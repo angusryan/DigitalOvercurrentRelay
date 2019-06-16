4ee0c0dfabb2406d99df334f6d580e7cbe2e8018
Ryan Angus <12947533@student.uts.edu.au>
2019-06-17 00:25:08 +1000

    Added structs and enums to header file

--- Sources/sample.h
+++ Sources/sample.h
@@ -1,1 +1,43 @@
 //Perform calculations on samples to determine characteristics
+
+typedef enum
+{
+  INVERSE,
+  VERY_INVERSE,
+  EXTREMELY_INVERSE
+} TIDMTCharacteristic;
+
+typedef enum {
+  a,
+  b,
+  c
+}TFaultType;
+
+typedef struct {
+  float Voltage;
+  float voltageRMS;
+  float currentRMS;
+
+  uint16union_t frequency;
+  uint8_t current;
+  uint8_t numberOfTrips;
+  TIDMTCharacteristic IDMTCharacteristic;
+  TFaultType faultType;
+
+}TSample;
+
+void Sample_Init(void) {
+
+}
+
+void Sliding_Voltage() {
+
+}
+
+void Current_RMS() {
+
+}
+
+void True_RMS(float Voltage) {
+
+}
